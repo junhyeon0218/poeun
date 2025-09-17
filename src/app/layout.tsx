8207aa1e81817bab4f14aed5c9bddbba485d88dc
junhyeon0218 <junhyeon0218@gmail.com>
2025-09-17 22:56:31 +0900

    아이콘 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from 'next';
 import '@/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'poeun',
-  description: 'poeun',
+  title: '김준현 | 포트폴리오',
+  description: '프론트엔드 개발자 김준현의 포트폴리오',
+  icons: {
+    icon: '/images/logo.png',
+  },
 };
 
 export default function RootLayout({
